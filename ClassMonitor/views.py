b9ad50de9a9cbf68182a84212945a26598b8ec80
TheArchons <progamer9138@gmail.com>
2023-01-22 11:39:36 -0800

    basic login

--- ClassMonitor/views.py
+++ ClassMonitor/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 
 
 def index(request):
     return render(request, './ClassMonitor/index.html')
+
+
+def login(request):
+    if request.method == 'POST':
+        print(request.POST['username'])
+        print(request.POST['password'])
+        return HttpResponse("Logged in")
+
+    return render(request, './ClassMonitor/login.html')
