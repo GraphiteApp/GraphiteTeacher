73d4bce9dd4c3f50f43bc7c871b446f9265721b6
TheArchons <progamer9138@gmail.com>
2023-01-25 11:36:01 -0800

    add students

--- ClassMonitor/models.py
+++ ClassMonitor/models.py
@@ -2,6 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class Student(models.Model):
+    username = models.CharField(max_length=20, unique=True, null=False, blank=False)
+    teacher = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
+
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, unique=True, null=False, blank=False)
     classCode = models.CharField(max_length=10, unique=True, null=False, blank=False)
+    examStarted = models.BooleanField(default=False)
+    students = models.ManyToManyField(Student, blank=True)
