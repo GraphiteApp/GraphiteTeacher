54f8d27890233464ac819721fa4028e76f72424e
TheArchons <progamer9138@gmail.com>
2023-01-25 10:29:14 -0800

    show profiles on admin

--- ClassMonitor/models.py
+++ ClassMonitor/models.py
@@ -3,5 +3,5 @@ from django.contrib.auth.models import User
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, unique=True, null=False, blank=False)
     classCode = models.CharField(max_length=10, unique=True, null=False, blank=False)
